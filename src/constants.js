0ed49b477025a839b50d2949e75e76a71f74a7b4
Kyle Morgan <kmorgan8588@gmail.com>
2020-06-15 17:05:14 -0700

    add the last helper functions for handling eating, pooping, dying
    Fixed a bug where when it rains, the sprite would not face the correct direction

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,15 @@
-export const TICK_RATE = 3000;
+export const TICK_RATE = 2000;
 export const ICONS = ["fish", "poop", "weather"];
+export const RAIN_CHANCE = 1;
+export const SCENES = ["day", "rain"];
+export const DAY_LENGTH = 45;
+export const NIGHT_LENGTH = 10;
+
+export const getNextHungerTime = (clock) =>
+  Math.floor(Math.random() * 3) + 5 + clock;
+
+export const getNextDieTime = (clock) =>
+  Math.floor(Math.random() * 3) + 3 + clock;
+
+export const getNextPoopTime = (clock) =>
+  Math.floor(Math.random() * 2) + 5 + clock;
--- src/gameState.js
+++ src/gameState.js
@@ -1,10 +1,43 @@
+import { modFox, modScene, togglePoopBag, writeModal } from "./ui";
+import {
+  RAIN_CHANCE,
+  SCENES,
+  DAY_LENGTH,
+  NIGHT_LENGTH,
+  getNextDieTime,
+  getNextHungerTime,
+  getNextPoopTime,
+} from "./constants";
+
 const gameState = {
   current: "INIT",
   clock: 1,
   wakeTime: -1,
+  sleepTime: -1,
+  hungryTime: -1,
+  dieTime: -1,
+  poopTime: -1,
+  timeToStartCelebrating: -1,
+  timeToEndCelebrating: -1,
   tick() {
     this.clock++;
-    console.log(`Clock ${this.clock}`);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    } else if (this.clock === this.sleepTime) {
+      this.sleep();
+    } else if (this.clock === this.hungryTime) {
+      this.getHungry();
+    } else if (this.clock === this.dieTime) {
+      this.die();
+    } else if (this.clock === this.timeToStartCelebrating) {
+      this.celebrate();
+    } else if (this.clock === this.timeToEndCelebrating) {
+      this.endCelebrating();
+    } else if (this.clock === this.poopTime) {
+      this.poop();
+    }
+
     return this.clock;
   },
   handleUserAction(icon) {
@@ -29,24 +62,99 @@ const gameState = {
         break;
     }
   },
-  changeWeather() {
-    console.log("change weather");
+  sleep() {
+    this.current = "SLEEP";
+    modFox("sleep");
+    modScene("night");
+    this.clearTimes();
+    this.wakeTime = this.clock + NIGHT_LENGTH;
+  },
+  poop() {
+    this.current = "POOPING";
+    this.poopTime = -1;
+    this.dieTime = getNextDieTime(this.clock);
+    modFox("pooping");
   },
   cleanUpPoop() {
-    console.log("clean poopoo");
+    if (this.current !== "POOPING") return;
+
+    this.dieTime = -1;
+    togglePoopBag(true);
+    this.celebrate();
+    this.hungryTime = getNextHungerTime(this.clock);
+  },
+  changeWeather() {
+    this.scene = (this.scene + 1) % SCENES.length;
+    modScene(SCENES[this.scene]);
+    this.determineFoxState();
+  },
+  celebrate() {
+    this.current = "CELEBRATING";
+    modFox("celebrate");
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = this.clock + 2;
+  },
+  endCelebrating() {
+    this.timeToEndCelebrating = -1;
+    this.current = "IDLING";
+    this.determineFoxState();
+    togglePoopBag(false);
+  },
+  determineFoxState() {
+    if (this.current === "IDLING") {
+      if (SCENES[this.scene] === "rain") {
+        modFox("rain");
+      } else {
+        modFox("idling");
+      }
+    }
   },
   feed() {
-    console.log("feast on fish");
+    if (this.current !== "HUNGRY") return;
+
+    this.current = "FEEDING";
+    this.dieTime = -1;
+    modFox("eating");
+    this.poopTime = getNextPoopTime(this.clock);
+    this.timeToStartCelebrating = this.clock + 3;
   },
   startGame() {
-    console.log("hatching");
     this.current = "HATCHING";
-    this.wakeTime = this.clock + 4;
+    this.wakeTime = this.clock + 3;
+    modFox("egg");
+    modScene("day");
+    writeModal();
   },
   wake() {
-    console.log("wake up!");
     this.current = "IDLING";
     this.wakeTime = -1;
+    this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
+    modScene(SCENES[this.scene]);
+    this.determineFoxState();
+    this.sleepTime = this.clock + DAY_LENGTH;
+    this.hungryTime = getNextHungerTime(this.clock);
+  },
+  getHungry() {
+    this.current = "HUNGRY";
+    this.dieTime = getNextDieTime(this.clock);
+    this.hungryTime = -1;
+    modFox("hungry");
+  },
+  die() {
+    this.current = "DEAD";
+    modFox("dead");
+    modScene("dead");
+    this.clearTimes();
+    writeModal("The fox died :( <br/> Press the middle button to restart");
+  },
+  clearTimes() {
+    this.wakeTime = -1;
+    this.sleepTime = -1;
+    this.hungryTime = -1;
+    this.poopTime = -1;
+    this.dieTime = -1;
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = -1;
   },
 };
 
