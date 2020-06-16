8d179cbfc504c5a57f49780d6460b0a3df7749de
Kyle Morgan <kmorgan8588@gmail.com>
2020-06-15 17:06:34 -0700

    returned constant RAIN_CHANCE back to .2

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const TICK_RATE = 2000;
 export const ICONS = ["fish", "poop", "weather"];
-export const RAIN_CHANCE = 1;
+export const RAIN_CHANCE = 0.2;
 export const SCENES = ["day", "rain"];
 export const DAY_LENGTH = 45;
 export const NIGHT_LENGTH = 10;
